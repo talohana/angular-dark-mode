a1ef3752d5fae7a7e3addacf690c1cb8b5d0cb27
talohana <tal.ohana.x@gmail.com>
2020-10-04 23:19:05 +0300

    chore(semantic-release): added tags to trigger builds

--- release.config.js
+++ release.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'eslint',
+        releaseRules: [
+          { tag: 'docs', release: 'patch' },
+          { tag: 'chore', release: 'patch' },
+          { tag: 'refactor', release: 'patch' },
+        ],
+      },
+    ],
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
