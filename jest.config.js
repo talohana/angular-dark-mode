8938fe8d93eb5a36cf178cbab2593229dd45212c
Tal Ohana <tal.ohana.x@gmail.com>
2023-03-24 12:05:12 +0300

    feat: support angular v13 (#42)
    * feat: support Angular 13
    
    * chore(test): update to latest jest version
    
    * chore: add devcontainer config and update node version to v18
    
    re #41

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 module.exports = {
   preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts', '<rootDir>/test.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFiles: ['jest-localstorage-mock'],
-  setupFilesAfterEnv: ['<rootDir>/test.ts'],
   collectCoverage: true,
   coverageDirectory: './coverage/',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/',
   }),
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
 };
